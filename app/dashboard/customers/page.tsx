6d38433227af597e611c967059e27c60e554ee1d
Oscar Meneses <omenesess1800@alumno.ipn.mx>
2023-12-21 22:33:18 -0600

    Add customer search functionality to dashboard page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,22 @@
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: 'Customers'
 };
-export default function Page() {
+export default async function Page({
+    searchParams,
+  }: {
+    searchParams?: {
+      query?: string;
+    };
+  }) {
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
+    console.log(customers);
     return (
-        <div>Customers</div>
+        <CustomersTable customers={customers} />
     )
 }
