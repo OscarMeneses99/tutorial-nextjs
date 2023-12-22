caa4c2350aa079f4857d0634cf4c4763b32a9655
Oscar Meneses <omenesess1800@alumno.ipn.mx>
2023-12-22 00:49:29 -0600

    Add login details and information icon to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,10 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
 import { Metadata } from 'next';
- 
+
 export const metadata: Metadata = {
-  title: 'Login'
+    title: 'Login'
 };
 
 export default function LoginPage() {
@@ -16,6 +17,17 @@ export default function LoginPage() {
                     </div>
                 </div>
                 <LoginForm />
+                <div className='w-full bg-slate-100 rounded p-10 flex flex-col justify-center items-center'>
+                    <p className='flex'>
+                        <InformationCircleIcon className='w-6 h-6 text-yellow-500' />
+                        <span className='ml-1 font-bold text-xl'>Accessing the App</span>
+                    </p>
+                    <p className='font-semibold text-lg'>Here are the login details:</p>
+                    <div>
+                        <p><strong>Username: </strong>user@nextmail.com</p>
+                        <p><strong>Password: </strong>123456</p>
+                    </div>
+                </div>
             </div>
         </main>
     );
